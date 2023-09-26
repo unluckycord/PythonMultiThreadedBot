8fd4b6c8f878b4c2696c0ee4444a24a8ec7995cf
unluckycord <loganknapp2@gmail.com>
2023-09-25 17:26:57 -0700

    bot can log in and perform actions

--- Task.py
+++ Task.py
@@ -12,10 +12,16 @@ from selenium.webdriver.common.by import By
 def Access_Accounts():
     driver = webdriver.Chrome()
     driver.get("https://www.instagram.com")
+    #print(driver.page_source)
     driver.implicitly_wait(20)
     username = driver.find_element(by=By.NAME, value = "username")
     username.clear
-    username.send_keys("testemail@gmail.com")
+    username.send_keys('username')
     password = driver.find_element(by=By.NAME, value = "password")
     password.clear
-    password.send_keys("password")
+    password.send_keys('password')
+    driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div').click()
+    driver.implicitly_wait(60)
+    driver.find_element(By.XPATH, '/html/body')
+    driver.find_element(By.XPATH, '//*[@id="mount_0_0_VR"]/div/div/div[2]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/span/div/a/div/div[1]/div/div/svg/path').click()
+    #driver.find_element(By.NAME, "Log in").click()
